420913e66e3d6a123992f0352cd837556a0ef7b2
zsphS <zsphS@WINPENNINGNITRO>
2023-07-03 14:05:42 -0300

    Adicionar arquivos de projeto.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+       public IActionResult New()
+        {
+            ViewData["Message"] = "Bem vindo a página new!";
+            ViewData["Nome"] = "Paulo Henrique Ziemer dos Santos";
+            ViewData["Curso"] = "Análise e Desenvolvimento de Sistemas";
+            return View();
+        }
+    }
+}
